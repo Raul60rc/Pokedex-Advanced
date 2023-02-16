7b3ea15434050c4506c6ffa366ac17f96d3fbdaf
Raul60rc <34682876+Raul60rc@users.noreply.github.com>
2023-02-16 15:01:36 +0100

    Js Script fetched  & pokemons painted with CSS

---
+++ main.js
@@ -0,0 +1,49 @@
+const pokemonContainer = document.querySelector('.pokemon-container')
+const spinner = document.querySelector('#spinner')
+
+function fetchPokemon(id){
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    .then(res => res.json())
+    .then((data) =>{
+        createPokemon(data);
+        spinner.style.display = "none";
+    }
+    )};
+
+function fetchPokemons(number) {
+    spinner.style.display = "block";
+ for (let index = 1; index < number; index++) {
+    fetchPokemon(index)
+    }
+}
+
+
+function createPokemon(pokemon){ //DOM 
+    const card = document.createElement("div");
+    card.classList.add('pokemon-block');
+
+    const spriteContainer = document.createElement('div'); // creates a div for every sprite container will contain image
+    spriteContainer.classList.add('img-container')
+
+    const sprite = document.createElement('img')
+    sprite.src = pokemon.sprites.front_default // it has this property in the sprite
+
+    spriteContainer.appendChild(sprite);  
+
+    const number = document.createElement('p');
+    number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`; //
+
+    const name = document.createElement('p');
+    name.classList.add ('name');
+    textContent = pokemon.name;
+
+    card.appendChild(spriteContainer);
+    card.appendChild(number);
+    card.appendChild(name);
+
+    pokemonContainer.appendChild(card);
+    //appenchild adds DOM to html 
+
+}
+
+fetchPokemons(152);
